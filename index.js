bd7ab8d458c1e43a927377cdc27ded5db08f2100
kautukdwivedi <kautukdwivedi@icloud.com>
2021-11-03 21:59:00 +0530

    blog

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 let port = process.env.PORT || 3000;
 
 import dataRoutes from './routes/data.js'
+import blogRoutes from './routes/blog.js'
 
 app.use('/data', dataRoutes);
 
@@ -10,6 +11,7 @@ app.get('/', (req, res) => {
     res.send('Medium Crawler API')
 })
 
+app.use('/id', blogRoutes);
 app.listen(port, () => {
     console.log('server started at ', port);
 });
---
+++ routes/blog.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+import getArticle from '../src/article.js'
+
+router.get('/:id', async (req, res) => {
+    let blog = await getArticle(req.params.id);
+    res.send(blog);
+});
